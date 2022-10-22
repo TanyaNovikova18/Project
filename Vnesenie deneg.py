610d4ca24c03eb5a00384654beffdd6452128927
TanyaNovikova18 <114173681+TanyaNovikova18@users.noreply.github.com>
2022-10-22 12:30:10 +0500

    1

--- Vnesenie deneg.py
+++ Vnesenie deneg.py
@@ -1,5 +1,5 @@
 import datetime
-f = open ('Vvod_deneg.txt', 'r+')
+file = open ('Vnesenie_deneg.txt', 'a+', encoding='utf-8')
 print("Каким способом вы хотите пополнить счёт?")
 print("1. Банковской картой \n"
       "2. С мобильного счёта \n"
@@ -11,30 +11,30 @@ if (sposob == 1):
       sumw = float(input("Введите сумму (руб.): "))
       if (sumw >= 1.00 and sumw <= 15000.00):
           year = datetime.datetime.today()
-          f.write(str(year)[:16], "  Банковской картой  ", sumw, "\n")
+          file.write(str(year)[:16]+ "; Банковской картой; ", str(sumw) + "\n")
       else:
             print("Неправильная сумма")
 elif (sposob == 2):
       sumw = int(input("Введите сумму (руб.): "))
       if (sumw >= 10.00 and sumw <= 50000.00):
             year = datetime.datetime.today()
-            f.write(str(year)[:16], " Мобильный счёт ", sumw, "\n")
+            file.write(str(year)[:16] + "; Мобильный счёт; " + str(sumw) + "\n")
       else:
             print("Неправильная сумма")
 elif (sposob == 3):
       sumw = float(input("Введите сумму (руб.): "))
       if (sumw >= 50 and sumw <= 10000000 and sum % 50 == 0):
             year = datetime.datetime.today()
-            f.write(str(year)[:16], " Банкомат ", sumw, "\n")
+            file.write(str(year)[:16] + "; Банкомат; " + str(sumw) + "\n")
       else:
             print("Неправильная сумма")
 elif (sposob == 4):
       sumw = float(input("Введите сумму (руб.): "))
       if (sumw >= 1.00 and sumw <= 500000.00):
             year = datetime.datetime.today()
-            f.write(str(year)[:16], " С другого электронного кошелька ", sumw, "\n")
+            file.write(str(year)[:16] + "; С другого электронного кошелька; " + str(sumw) + "\n")
       else:
             print("Неправильная сумма")
 else:
       print("Неверный ответ")
-f.close()
+file.close()
